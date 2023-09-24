08ac5b0f9b0be910d456d63e2ad40c1fdc47b52c
EMPTY280 <1123pjs@gmail.com>
2023-09-24 21:41:21 +0900

    First commit

---
+++ Assets/LevelSegment.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSegment : MonoBehaviour
+{
+    // 에디터에서의 기본 위치.
+    private Vector2 originPos = Vector2.zero;
+
+    private void Awake()
+    {
+        originPos = transform.position;
+    }
+
+    // 에디터에서 가상의 사각형 (맵 세그먼트 범위 표시)
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(22f, 10f, 1f));
+    }
+#endif
+
+    // 사용이 끝나면 최초 위치로 복귀
+    public void ReturnToOrigin()
+    {
+        transform.position = originPos;
+    }
+}
---
+++ Assets/MapScroller.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScroller : MonoBehaviour
+{
+    // 레벨 조각 리스트
+    List<LevelSegment> segments = new List<LevelSegment>();
+
+    // 시작시 최초로 배치할 조각의 인덱스
+    [SerializeField] int firstSegment = 0;
+
+    // 현재 화면의 조각 / 다음 화면의 조각
+    LevelSegment currentSegment = null;
+    LevelSegment nextSegment = null;
+
+    // 스크롤 속도
+    [SerializeField] float scrollSpeed = 1.0f;
+    // 현재 스크롤중인 조각의 위치 및 이동 임계값
+    float offset = 0.0f;
+    float offsetMax = 22.0f;
+
+
+    private void Awake()
+    {
+        // 세그먼트 리스트에 넣기
+        int count = transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            LevelSegment seg = transform.GetChild(i).GetComponent<LevelSegment>();
+            segments.Add(seg);
+        }
+
+        // 최초 조각 + 두번째 조각 로드
+        currentSegment = segments[firstSegment];
+        segments.RemoveAt(firstSegment);
+        nextSegment = GetRandomSegment();
+
+        SetSegmentPos();
+    }
+
+    // 조각 스크롤 / 조각이 화면 밖으로 나갔다면 다음 조각 로드
+    private void FixedUpdate()
+    {
+        offset += scrollSpeed;
+
+        if (offset >= offsetMax)
+        {
+            offset -= offsetMax;
+            LoadNewSegment();
+        }
+
+        SetSegmentPos();
+    }
+
+    // 리스트에서 무작위 조각을 POP
+    private LevelSegment GetRandomSegment()
+    {
+        int count = segments.Count;
+        int randomIdx = Random.Range(0, count);
+
+        LevelSegment select = segments[randomIdx];
+        segments.RemoveAt(randomIdx);
+
+        return select;
+    }
+
+    // 각 조각의 위치 업뎅트
+    private void SetSegmentPos()
+    {
+        currentSegment.transform.position = new Vector2(0 - offset, 0);
+        nextSegment.transform.position = new Vector2(22 - offset, 0);
+    }
+
+    // 현재 사용중인 조각 업데이트
+    private void LoadNewSegment()
+    {
+        currentSegment.ReturnToOrigin();
+        segments.Add(currentSegment);
+        currentSegment = nextSegment;
+        nextSegment = GetRandomSegment();
+    }
+}
